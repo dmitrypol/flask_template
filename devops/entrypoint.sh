11a6da0d6a73dde315b304cf9bc55f3f24039db2
Dmitry Polyakovsky <dmitry.polyakovsky@oracle.com>
2020-08-04 07:41:16 -0700

    minor tweak

--- devops/entrypoint.sh
+++ devops/entrypoint.sh
@@ -5,12 +5,12 @@ set -m
 flask run -h 0.0.0.0 -p 5000 --reload &
 
 # https://ryanstutorials.net/bash-scripting-tutorial/bash-if-statements.php
-if [ $CONTAINER_TYPE = 'web' ]
-then
-    # run web here
-elif [ $CONTAINER_TYPE = 'worker' ]
-    # run worker here
-then
-fi
+# if [ $CONTAINER_TYPE = 'web' ]
+# then
+#     # run web here
+# elif [ $CONTAINER_TYPE = 'worker' ]
+# then
+#     # run worker here
+# fi
 
 fg %1
